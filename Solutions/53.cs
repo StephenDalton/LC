8cfaa18140b52db316a0cc1ca0b40e46c746e168
Stephen Dalton <stephen.dalt@gmail.com>
2022-03-30 07:16:09 -0400

    Update 53.cs

--- Solutions/53.cs
+++ Solutions/53.cs
@@ -1,1 +1,17 @@
-test
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+        int max = nums[0];
+        int currentSum = 0;
+        
+        for(int i=0; i<nums.Length; i++)
+        {
+            if (currentSum < 0)
+                currentSum = 0;
+            
+            currentSum += nums[i];
+            max = Math.Max(max, currentSum);
+            
+        }
+        return max;
+    }
+}
