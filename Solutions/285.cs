2ab19b7d98c818a520152c32cdca239f8236be33
Stephen Dalton <stephen.dalt@gmail.com>
2022-04-01 07:50:15 -0400

    Update 285.cs

--- Solutions/285.cs
+++ Solutions/285.cs
@@ -1,1 +1,29 @@
-test
+//https://leetcode.com/problems/inorder-successor-in-bst/discuss/72656/JavaPython-solution-O(h)-time-and-O(1)-space-iterative
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode InorderSuccessor(TreeNode root, TreeNode p) {
+        
+        TreeNode successor = null;
+        
+        while (root != null) 
+        {
+            if (p.val < root.val) 
+            {
+                successor = root;
+                root = root.left;
+            }
+            else
+                root = root.right;
+        }
+        
+        return successor;
+    }
+}
