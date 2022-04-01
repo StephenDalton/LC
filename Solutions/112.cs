0161ffb612569eb7061fbce0f8518ea37a9edaf3
Stephen Dalton <stephen.dalt@gmail.com>
2022-04-01 15:02:04 -0400

    Update 112.cs

--- Solutions/112.cs
+++ Solutions/112.cs
@@ -1,1 +1,29 @@
-test
+//https://leetcode.com/problems/path-sum/discuss/36378/AcceptedMy-recursive-solution-in-Java
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasPathSum(TreeNode root, int targetSum) 
+    {
+        if (root == null)
+            return false;
+        
+        if (root.left == null && root.right == null && root.val == targetSum)
+            return true;
+        
+        bool left = HasPathSum(root.left, targetSum - root.val);
+        bool right = HasPathSum(root.right, targetSum - root.val);
+        
+        return left || right;
+    }
+}
