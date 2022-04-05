47727fd4645b787bd1a37d8a10e12418c5e600f2
Stephen Dalton <stephen.dalt@gmail.com>
2022-04-05 15:43:15 -0400

    Update 22.cs

--- Solutions/22.cs
+++ Solutions/22.cs
@@ -1,1 +1,21 @@
-test
+//https://leetcode.com/problems/generate-parentheses/discuss/10100/Easy-to-understand-Java-backtracking-solution
+public IList<string> GenerateParenthesis(int n) {
+    List<string> results = new List<string>();
+
+    Backtrack(n, "", results, 0, 0);
+    return results;
+}
+
+private void Backtrack(int n, string current, List<string> results, int openCount, int closeCount) {
+    if (current.Length == n * 2)
+    {
+        results.Add(current);
+        return;
+    }
+
+    if (openCount < n)
+        Backtrack(n, current + "(", results, openCount + 1, closeCount);
+
+    if (closeCount < openCount)
+        Backtrack(n, current + ")", results, openCount, closeCount + 1);
+}
