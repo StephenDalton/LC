cd099691613b953c71f7ffe572f5c683e43581f5
Stephen Dalton <stephen.dalt@gmail.com>
2022-04-05 11:56:17 -0400

    Update 784.cs

--- Solutions/784.cs
+++ Solutions/784.cs
@@ -1,1 +1,29 @@
-test
+//https://leetcode.com/problems/letter-case-permutation/discuss/255071/Java-detailed-explanation-of-DFSBacktracking-solution
+public class Solution {
+    public IList<string> LetterCasePermutation(string s) {
+        List<string> result = new List<string>();
+        
+        Backtrack(result, 0, s.ToCharArray());
+        
+        return result;
+    }
+    
+    private void Backtrack(List<string> result, int index, char[] s){
+        if (index == s.Length)
+        {    
+            result.Add(new string(s));
+            return;
+        }
+      
+        if (char.IsLetter(s[index]))
+        {
+            s[index] = char.ToLower(s[index]);
+            Backtrack(result, index + 1, s);
+            
+            s[index] = char.ToUpper(s[index]);
+            Backtrack(result, index + 1, s);
+        }
+        else
+            Backtrack(result, index + 1, s);
+    }
+}
