68ea614838ae424d909559f7d30cc85c15c0fd9e
Stephen Dalton <stephen.dalt@gmail.com>
2022-04-06 08:30:14 -0400

    Update 320.cs

--- Solutions/320.cs
+++ Solutions/320.cs
@@ -1,1 +1,29 @@
-test
+//https://leetcode.com/problems/generalized-abbreviation/discuss/77190/Java-backtracking-solution
+public class Solution {
+    public IList<string> GenerateAbbreviations(string word) {
+        List<string> results = new List<string>();
+        
+        Backtrack(word, "", results, 0, 0);
+        return results;
+    }
+
+    private void Backtrack(string word, string current, List<string> results,  int index, int count){
+        if(index == word.Length)
+        {
+            if(count > 0) 
+                current += count;
+            
+            results.Add(current);
+            return;
+        }
+
+        Backtrack(word, current, results, index + 1, count + 1);
+        
+        if (count > 0)
+            current = current + count + word[index];
+        else
+            current = current + word[index];
+        
+        Backtrack(word, current, results, index + 1, 0);
+    }
+}
