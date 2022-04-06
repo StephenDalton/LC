d06cfdd2f0eb5f1f676e27c1c4545884f3a31116
Stephen Dalton <stephen.dalt@gmail.com>
2022-04-06 14:36:41 -0400

    Update 704.cs

--- Solutions/704.cs
+++ Solutions/704.cs
@@ -1,1 +1,23 @@
-test
+public class Solution {
+    public int Search(int[] nums, int target) {
+        
+        if (nums.Length == 0 || nums == null)
+            return -1;
+        
+        int low = 0;
+        int high = nums.Length - 1;
+        
+        while (low <= high)
+        {
+            int mid = low + (high-low) / 2;
+            
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+}
