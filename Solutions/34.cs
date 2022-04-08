69f763dcf80440182644fb87f8fca28e3160fd01
Stephen Dalton <stephen.dalt@gmail.com>
2022-04-08 07:40:46 -0400

    Update 34.cs

--- Solutions/34.cs
+++ Solutions/34.cs
@@ -1,1 +1,46 @@
-test
+//https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/discuss/14734/Easy-java-O(logn)-solution
+public class Solution {
+    public int[] SearchRange(int[] nums, int target) {
+        return new int[] { FindFirst(nums, target), FindLast(nums, target) };
+    }
+    
+    private int FindFirst(int[] nums, int target) {
+        int idx = -1;
+        int low = 0;
+        int high = nums.Length - 1;
+        
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if(nums[mid] >= target)
+                high = mid - 1;
+            else
+                low = mid + 1;
+            
+            if(nums[mid] == target) 
+                idx = mid;
+        }
+        
+        return idx;
+    }
+    
+    private int FindLast(int[] nums, int target) {
+        int idx = -1;
+        int low = 0;
+        int high = nums.Length - 1;
+        
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if(nums[mid] <= target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+            
+            if(nums[mid] == target) 
+                idx = mid;
+        }
+        
+        return idx;
+    }
+}
