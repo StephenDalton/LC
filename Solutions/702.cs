031876560ba074367e5369f975bfb64afc934677
Stephen Dalton <stephen.dalt@gmail.com>
2022-04-08 09:30:53 -0400

    Update 702.cs

--- Solutions/702.cs
+++ Solutions/702.cs
@@ -1,1 +1,28 @@
-test
+//https://leetcode.com/problems/search-in-a-sorted-array-of-unknown-size/discuss/1801001/single-pass-binary-search
+/**
+ * // This is ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class ArrayReader {
+ *     public int Get(int index) {}
+ * }
+ */
+class Solution {
+    public int Search(ArrayReader reader, int target) {
+        int low = 0;
+        int high = Int32.MaxValue;
+        
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            
+            if (reader.Get(mid) == target)
+                return mid;
+            else if (reader.Get(mid) < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        
+        return -1;
+    }
+}
