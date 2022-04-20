59ae353f552195d3a4e1f8a3bf83358bfbf0a8c6
Stephen Dalton <stephen.dalt@gmail.com>
2022-04-20 10:21:03 -0400

    Update 1539.cs

--- Solutions/1539.cs
+++ Solutions/1539.cs
@@ -1,1 +1,20 @@
-test
+//https://leetcode.com/problems/kth-missing-positive-number/discuss/1687149/Java-Simple-Solution
+public class Solution {
+    public int FindKthPositive(int[] arr, int k) {
+        
+        int left = 0;
+        int right = arr.Length - 1;
+        
+        while (left <= right) 
+        {
+            int mid = left (right - left) / 2;
+            
+            if (arr[mid] - mid - 1 >= k) 
+                right = mid - 1;
+            else 
+                left = mid + 1;
+        }
+        
+        return k + left;
+    }
+}
