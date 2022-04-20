aaf29ccfc1d1627c9feabef110be5deefd5b0987
Stephen Dalton <stephen.dalt@gmail.com>
2022-04-20 11:22:24 -0400

    Update 136.cs

--- Solutions/136.cs
+++ Solutions/136.cs
@@ -1,1 +1,13 @@
-test
+//https://leetcode.com/problems/single-number/discuss/43171/1ms-JAVA-solution
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        int result = 0;
+        
+        foreach (int num in nums)
+        {
+            result = result ^ num;
+        }
+        
+        return result;
+    }
+}
