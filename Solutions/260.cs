76fd2cd967e01d24bb9825da507d2cd4c66119a8
Stephen Dalton <stephen.dalt@gmail.com>
2022-04-22 07:54:17 -0400

    Update 260.cs

--- Solutions/260.cs
+++ Solutions/260.cs
@@ -1,1 +1,15 @@
-test
+//https://leetcode.com/problems/single-number-ii/discuss/1501849/Easy-to-read-commented-and-explained-C%2B%2B-solution
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        int oneCounter = 0;
+        int twoCounter = 0;
+        
+        foreach (int n in nums) 
+        {
+            oneCounter = (oneCounter ^ n) & ~twoCounter;
+            twoCounter = (twoCounter ^ n) & ~oneCounter;
+        }
+        
+        return oneCounter;
+    }
+}
