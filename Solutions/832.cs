8544912ed2e0ec8ec29ce44c38ebb1261857bec6
Stephen Dalton <stephen.dalt@gmail.com>
2022-04-22 10:19:18 -0400

    Update 832.cs

--- Solutions/832.cs
+++ Solutions/832.cs
@@ -1,1 +1,16 @@
-test
+//https://leetcode.com/problems/flipping-an-image/discuss/282410/Java-one-pass
+public class Solution {
+    public int[][] FlipAndInvertImage(int[][] image) {
+        
+        int n = image.Length;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < (n + 1) / 2; j++) {
+                int temp = image[i][j];
+                image[i][j] = image[i][n - j - 1] ^ 1;
+                image[i][n - j - 1] = temp ^ 1;
+            }
+        }
+        return image;
+    }
+}
