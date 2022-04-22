a6653327c2761c96ee76d8e5ad5833b14e507b48
Stephen Dalton <stephen.dalt@gmail.com>
2022-04-22 08:04:11 -0400

    Update 1009.cs

--- Solutions/1009.cs
+++ Solutions/1009.cs
@@ -1,1 +1,17 @@
-test
+//https://leetcode.com/problems/complement-of-base-10-integer/discuss/258500/Java-0ms-32mb...-beats-100
+public class Solution {
+    public int BitwiseComplement(int n) {
+        
+        if (n == 0) return 1;
+        if (n == 1) return 0;
+        
+        int x = 1;
+        
+        while (x <= n)
+        {
+             x = x << 1;
+        }
+        
+        return n ^ (x-1);
+    }
+}
