581d3ddf7752fdfb693bad88993729aa6890db25
Stephen Dalton <stephen.dalt@gmail.com>
2022-04-27 14:58:17 -0400

    Update 217.cs

--- Solutions/217.cs
+++ Solutions/217.cs
@@ -1,1 +1,17 @@
-blank
+//https://leetcode.com/problems/contains-duplicate/discuss/1157635/C-solutions
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        
+        HashSet<int> set = new HashSet<int>();
+        
+        for (int i=0; i<nums.Length; i++)
+        {
+            if (set.Contains(nums[i]))
+                return true;
+            else 
+                set.Add(nums[i]);
+        }
+        
+        return false;
+    }
+}
