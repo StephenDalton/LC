a354732f9a95e309d60a3344d711ba46e1f15216
Stephen Dalton <stephen.dalt@gmail.com>
2022-04-27 14:47:15 -0400

    Update 121.cs

--- Solutions/121.cs
+++ Solutions/121.cs
@@ -1,1 +1,16 @@
-blank
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/discuss/1735493/JavaC%2B%2B-best-ever-EXPLANATION-could-possible
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        
+        int maxProfit = 0;
+        int minValue = Int32.MaxValue;
+        
+        for (int i=0; i<prices.Length; i++)
+        {
+            minValue = Math.Min(minValue, prices[i]);
+            maxProfit = Math.Max(maxProfit, prices[i] - minValue);
+        }
+        
+        return maxProfit;
+    }
+}
