c15e9b305925ad381555c2e34ac2229e1b217868
Stephen Dalton <stephen.dalt@gmail.com>
2022-05-02 07:55:24 -0400

    Update 53.cs

--- Solutions/53.cs
+++ Solutions/53.cs
@@ -1,1 +1,19 @@
-blank
+//https://leetcode.com/problems/maximum-subarray/discuss/1595097/JAVA-or-Kadane's-Algorithm-or-Explanation-Using-Image
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+        
+        int max = nums[0];
+        int currentSum  = 0;
+        
+        for (int i=0; i<nums.Length; i++)
+        {
+            if (currentSum < 0)
+                currentSum = 0;
+            
+            currentSum += nums[i];
+            max = Math.Max(max, currentSum);
+        }
+        
+        return max;
+    }
+}
