59907670173c00fc860e9104ab23bbbe48afda9d
Stephen Dalton <stephen.dalt@gmail.com>
2022-05-04 11:44:37 -0400

    Update 3.cs

--- Solutions/3.cs
+++ Solutions/3.cs
@@ -1,1 +1,28 @@
-blank
+//
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        
+        int max = 0;
+        
+        int start = 0;
+        int end = 0;
+        
+        int[] map = new int[256];
+        
+        while (end < s.Length)
+        {
+            map[s[end]]++;
+            end++;
+            
+            while (map[s[end - 1]] > 1)
+            {
+                map[s[start]]--;
+                start++;
+            }
+            
+            max = Math.Max(max, end - start);
+        }
+        
+        return max;
+    }
+}
