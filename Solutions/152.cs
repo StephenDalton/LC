5f84b6a0301f86295d45c8ba27d98ec8df5f19c4
Stephen Dalton <stephen.dalt@gmail.com>
2022-05-04 07:54:23 -0400

    Update 152.cs

--- Solutions/152.cs
+++ Solutions/152.cs
@@ -1,1 +1,22 @@
-blank
+//
+public class Solution {
+    public int MaxProduct(int[] nums) {
+        int max = Int32.MinValue;
+        
+        int currentMax = 1;
+        int currentMin = 1;
+        
+        foreach (var num in nums)
+        {
+            int productA = currentMax * num;
+            int productB = currentMin * num;
+            
+            currentMax = Math.Max(num, Math.Max(productA, productB));
+            currentMin = Math.Min(num, Math.Min(productA, productB));
+            
+            max = Math.Max(max, currentMax);
+        }
+        
+        return max;
+    }
+}
