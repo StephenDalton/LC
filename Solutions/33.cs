d1a3f2022c89286fe32974b529834ffaea155f5a
Stephen Dalton <stephen.dalt@gmail.com>
2022-05-04 09:42:11 -0400

    Update 33.cs

--- Solutions/33.cs
+++ Solutions/33.cs
@@ -1,34 +1,27 @@
-//https://leetcode.com/problems/search-in-rotated-sorted-array/discuss/14436/Revised-Binary-Search/191339
+//https://leetcode.com/problems/search-in-rotated-sorted-array/discuss/14435/Clever-idea-making-it-simple/14793
 public class Solution {
     public int Search(int[] nums, int target) {
-
+        
         int left = 0;
         int right = nums.Length - 1;
-
-        while (left <= right)
+        
+        while (left < right)
         {
             int mid = left + (right - left) / 2;
-
-            if (nums[left] <= nums[mid])
+                     
+            if ((nums[mid] < nums[0]) == (target < nums[0]))
             {
-                if (nums[mid] == target)
-                    return mid;
-                else if (nums[left] <= target && target <= nums[mid])
-                    right = mid - 1;
-                else
-                    left = mid + 1;
-            }
-            else
-            {
-                if (nums[mid] == target)
-                    return mid;
-                else if (nums[mid] <= target && target <= nums[right])
+                if (nums[mid] < target)
                     left = mid + 1;
                 else
-                    right = mid - 1;
+                    right = mid;
             }
+            else if (target < nums[0])
+                left = mid + 1;
+            else 
+                right = mid;
         }
-
-        return -1;
+        
+        return nums[left] == target ? left : -1;
     }
 }
