5fc29b2e21bb71c5f1aaf9f22673ca5fcaf5a6db
Stephen Dalton <stephen.dalt@gmail.com>
2022-05-04 10:52:47 -0400

    Update 11.cs

--- Solutions/11.cs
+++ Solutions/11.cs
@@ -1,1 +1,23 @@
-blank
+//
+public class Solution {
+    public int MaxArea(int[] height) {
+        
+        int max = 0;
+        
+        int left = 0;
+        int right = height.Length - 1;
+        
+        while (left < right)
+        {
+            int shortest = Math.Min(height[left], height[right]);
+            max = Math.Max(max, shortest * (right - left) );
+            
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        
+        return max;
+    }
+}
