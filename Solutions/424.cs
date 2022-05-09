7ff7578bfbf42a200ba000c9c19e3673f5a3f5de
Stephen Dalton <stephen.dalt@gmail.com>
2022-05-09 07:39:59 -0400

    Update 424.cs

--- Solutions/424.cs
+++ Solutions/424.cs
@@ -1,1 +1,29 @@
-blank
+//
+public class Solution {
+    public int CharacterReplacement(string s, int k) {
+        
+        int max = 0;
+        int mostFreq = 0;
+        int[] frequency = new int[256];
+        
+        int start = 0;
+        int end = 0;
+        
+        while (end < s.Length)
+        {
+            frequency[s[end]]++;
+            mostFreq = Math.Max(mostFreq, frequency[s[end]]);
+            end++;
+            
+            if ((end - start) - mostFreq > k)
+            {
+                frequency[s[start]]--;
+                start++;
+            }
+            
+            max = Math.Max(max, end - start);
+        }
+        
+        return max;
+    }
+}
