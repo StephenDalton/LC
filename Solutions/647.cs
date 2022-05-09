089535be8649f1629b6aecc4b7191bd42d0b459c
Stephen Dalton <stephen.dalt@gmail.com>
2022-05-09 14:27:40 -0400

    Update 647.cs

--- Solutions/647.cs
+++ Solutions/647.cs
@@ -1,1 +1,31 @@
-blank
+//
+public class Solution {
+    public int CountSubstrings(string s) {
+        int count = 0;
+        
+        for (int i=0; i<s.Length; i++)
+        {
+            count += ExpandFromCenter(s, i, i);
+            count += ExpandFromCenter(s, i - 1, i);
+        }
+        
+        return count;
+    }
+    
+    private int ExpandFromCenter(string s, int left, int right) {
+        int count = 0;
+        
+        while (left >= 0 && right < s.Length)
+        {
+            if (s[left] != s[right])
+                break;
+            
+            count++;
+            
+            left--;
+            right++;
+        }
+        
+        return count;
+    }
+}
