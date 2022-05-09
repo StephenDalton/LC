765f6b5c96862df0d1c908c5bdd9cd6d12318bc5
Stephen Dalton <stephen.dalt@gmail.com>
2022-05-09 09:32:01 -0400

    Update 125.cs

--- Solutions/125.cs
+++ Solutions/125.cs
@@ -1,1 +1,26 @@
-blank
+//
+public class Solution {
+    public bool IsPalindrome(string s) {
+        
+        s = s.ToLower();
+        
+        int left = 0;
+        int right = s.Length - 1;
+        
+        while (left < right)
+        {
+            while (left < right && !Char.IsLetterOrDigit(s[left]))
+                left++;
+            while (left < right && !Char.IsLetterOrDigit(s[right]))
+                right--;
+            
+            if (s[left] != s[right])
+                return false;
+            
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
