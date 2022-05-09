06017cf4c831784b5fe9756c2d7c147f06b14553
Stephen Dalton <stephen.dalt@gmail.com>
2022-05-09 15:53:17 -0400

    Update 21.cs

--- Solutions/21.cs
+++ Solutions/21.cs
@@ -1,1 +1,36 @@
-blank
+//
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode sentinalNode = new ListNode(0);
+        ListNode current = sentinalNode;
+        
+        while (list1 != null || list2 != null)
+        {
+            if (list2 == null || (list1 != null && list1.val <= list2.val))
+            {
+                current.next = list1;
+                current = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                current.next = list2;
+                current = list2;
+                list2 = list2.next;
+            }
+        }
+        
+        return sentinalNode.next;
+    }
+}
