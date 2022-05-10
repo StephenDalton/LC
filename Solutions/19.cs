c444f4ffc02550328f544316aef8b465ade888ae
Stephen Dalton <stephen.dalt@gmail.com>
2022-05-10 07:53:45 -0400

    Update 19.cs

--- Solutions/19.cs
+++ Solutions/19.cs
@@ -1,1 +1,37 @@
-blank
+//
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode RemoveNthFromEnd(ListNode head, int n) {
+        ListNode sentinalNode = new ListNode(0);
+        sentinalNode.next = head;
+        
+        ListNode slow = sentinalNode;
+        ListNode fast = sentinalNode;
+        
+        while (n > 0)
+        {
+            fast = fast.next;
+            n--;
+        }
+        
+        while (fast.next != null)
+        {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return sentinalNode.next;
+    }
+}
