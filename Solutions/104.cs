f82d164ba4ad04de974124559b01990e9a054e41
Stephen Dalton <stephen.dalt@gmail.com>
2022-05-10 09:08:25 -0400

    Update 104.cs

--- Solutions/104.cs
+++ Solutions/104.cs
@@ -1,1 +1,25 @@
-blank
+//
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int MaxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        
+        int left = MaxDepth(root.left) + 1;
+        int right = MaxDepth(root.right) + 1;
+        
+        return Math.Max(left, right);
+    }
+}
