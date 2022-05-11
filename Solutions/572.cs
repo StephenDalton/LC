2042a3e441ba028c211c509b7fc75aec022a8a37
Stephen Dalton <stephen.dalt@gmail.com>
2022-05-11 08:41:28 -0400

    Update 572.cs

--- Solutions/572.cs
+++ Solutions/572.cs
@@ -1,1 +1,38 @@
-blank
+//
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null)
+            return subRoot == null;
+        
+        return Check(root, subRoot) || IsSubtree(root.left, subRoot) || IsSubtree(root.right, subRoot);
+    }
+    
+    private bool Check(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null)
+            return true;
+        
+        if (root == null || subRoot == null)
+            return false;
+        
+        if (root.val != subRoot.val)
+            return false;
+        
+        bool left = Check(root.left, subRoot.left);
+        bool right = Check(root.right, subRoot.right);
+        
+        return left && right;
+    }
+}
