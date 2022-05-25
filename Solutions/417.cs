4e1948274102cdc03e1865a5b15eb103700637b1
Stephen Dalton <stephen.dalt@gmail.com>
2022-05-25 15:24:03 -0400

    Update 417.cs

--- Solutions/417.cs
+++ Solutions/417.cs
@@ -1,48 +1,48 @@
 //https://leetcode.com/problems/pacific-atlantic-water-flow/discuss/90733/Java-BFS-and-DFS-from-Ocean
 public class Solution {
     public IList<IList<int>> PacificAtlantic(int[][] heights) {
-        List<IList<int>> res = new List<IList<int>>();
+        List<IList<int>> results = new List<IList<int>>();
         
         int n = heights.Length;
         int m = heights[0].Length;
         
-        bool[,] pacificCanReach = new bool[n,m];
-        bool[,] atlanticCanReach = new bool[n,m];
-        
-        for(int i=0; i<m; i++)
-        {
-            DFS(heights, pacificCanReach, Int32.MinValue, i, 0);
-            DFS(heights, atlanticCanReach, Int32.MinValue, i, n-1);
-        }
+        bool[,] pacific = new bool[n,m];
+        bool[,] atlantic = new bool[n,m];
         
         for(int i=0; i<n; i++)
         {
-            DFS(heights, pacificCanReach, Int32.MinValue, 0, i);
-            DFS(heights, atlanticCanReach, Int32.MinValue, m-1, i);
+            DFS(heights, pacific, Int32.MinValue, i, 0);
+            DFS(heights, atlantic, Int32.MinValue, i, m-1);
+        }
+        
+        for(int i=0; i<m; i++)
+        {
+            DFS(heights, pacific, Int32.MinValue, 0, i);
+            DFS(heights, atlantic, Int32.MinValue, n-1, i);
         }
         
         for (int i = 0; i < n; i++) 
         {
             for (int j = 0; j < m; j++) 
             {
-                if (pacificCanReach[i,j] && atlanticCanReach[i,j]) 
-                    res.Add(new List<int> {i, j});
+                if (pacific[i,j] && atlantic[i,j]) 
+                    results.Add(new List<int> {i, j});
             }
         }
             
-        return res;
+        return results;
     }
     
     
-    private void DFS(int[][] matrix, bool[,] canReach, int height, int x, int y) {
-        if (x<0 || x>=matrix[0].Length || y<0 || y>=matrix.Length || canReach[y,x] || matrix[y][x] < height)
+    private void DFS(int[][] heights, bool[,] canReach, int height, int i, int j) {
+        if (i < 0 || i >= heights.Length || j < 0 || j >= heights[0].Length || canReach[i,j] || heights[i][j] < height)
             return;
         
-        canReach[y,x] = true;
+        canReach[i,j] = true;
         
-        DFS(matrix, canReach, matrix[y][x], x+1, y);
-        DFS(matrix, canReach, matrix[y][x], x-1, y);
-        DFS(matrix, canReach, matrix[y][x], x, y+1);
-        DFS(matrix, canReach, matrix[y][x], x, y-1);
+        DFS(heights, canReach, heights[i][j], i+1, j);
+        DFS(heights, canReach, heights[i][j], i-1, j);
+        DFS(heights, canReach, heights[i][j], i, j+1);
+        DFS(heights, canReach, heights[i][j], i, j-1);
     }
 }
