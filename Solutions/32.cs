cccba4fa906f4266b499679fbe2a026e42c8093d
Stephen Dalton <stephen.dalt@gmail.com>
2022-05-25 09:31:46 -0400

    Update 32.cs

--- Solutions/32.cs
+++ Solutions/32.cs
@@ -1,1 +1,21 @@
-blank
+//
+public class Solution {
+    public int LongestValidParentheses(string s) {
+        Stack<int> stack = new Stack<int>();
+        int result = 0;
+        stack.Push(-1);
+        
+        for (int i=0; i<s.Length; i++) 
+        {
+            if (s[i] == ')' && stack.Count > 1 && s[stack.Peek()] == '(') 
+            {
+                stack.Pop();
+                result = Math.Max(result, i - stack.Peek());
+            }
+            else 
+                stack.Push(i);
+        }
+        
+        return result;
+    }
+}
