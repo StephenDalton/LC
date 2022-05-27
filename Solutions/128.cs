1738ba8d6dc95acf67ee381e80472d097477cc5b
Stephen Dalton <stephen.dalt@gmail.com>
2022-05-27 12:10:47 -0400

    Update 128.cs

--- Solutions/128.cs
+++ Solutions/128.cs
@@ -1,1 +1,25 @@
-blank
+//
+public class Solution {
+    public int LongestConsecutive(int[] nums) {        
+        int max = 0;
+        HashSet<int> set = new HashSet<int>();
+        
+        foreach (int num in nums)
+            set.Add(num);
+        
+        foreach (int num in set)
+        {
+            if (set.Contains(num - 1))
+                continue;
+
+            int count = 1;
+            
+            while (set.Contains(num + count))
+                count++;
+            
+            max = Math.Max(max, count);        
+        }
+        
+        return max;
+    }
+}
