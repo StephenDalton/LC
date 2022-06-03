54eae7ef13a81a94841728a9247e8acf528c04a6
Stephen Dalton <stephen.dalt@gmail.com>
2022-06-03 11:56:39 -0400

    Update 88.cs

--- Solutions/88.cs
+++ Solutions/88.cs
@@ -1,1 +1,31 @@
-blank
+//
+public class Solution {
+    public void Merge(int[] nums1, int m, int[] nums2, int n) {
+        int idx1 = m - 1;
+        int idx2 = n - 1;
+        int insertIndex = m + n - 1;
+        
+        while (idx1 >= 0 && idx2 >= 0)
+        {
+            if (nums1[idx1] >= nums2[idx2])
+            {
+                nums1[insertIndex] = nums1[idx1];
+                idx1--;
+            }
+            else
+            {
+                nums1[insertIndex] = nums2[idx2];
+                idx2--;
+            }
+            
+            insertIndex--;
+        }
+        
+        while (idx2 >= 0)
+        {
+            nums1[insertIndex] = nums2[idx2];
+            idx2--;
+            insertIndex--;
+        }
+    }
+}
