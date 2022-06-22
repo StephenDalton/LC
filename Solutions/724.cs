45dbcc67cab54da230a7510263fbfe671c5142ab
Stephen Dalton <stephen.dalt@gmail.com>
2022-06-22 11:02:35 -0400

    Update 724.cs

--- Solutions/724.cs
+++ Solutions/724.cs
@@ -1,1 +1,30 @@
-blank
+//https://leetcode.com/problems/find-pivot-index/discuss/109249/Java-6-liner
+
+
+/*  Extra space
+
+public class Solution {
+    public int PivotIndex(int[] nums) {
+
+        int[] leftSum = new int[nums.Length];
+        int[] rightSum = new int[nums.Length];
+        
+        leftSum[0] = 0;
+        rightSum[nums.Length - 1] = 0;
+        
+        for (int i=1; i<nums.Length; i++)
+        {
+            leftSum[i] = nums[i - 1] + leftSum[i - 1];
+            rightSum[nums.Length - i - 1] = nums[nums.Length - i] + rightSum[nums.Length - i];
+        }
+        
+        for (int i=0; i<nums.Length; i++)
+        {
+            if (leftSum[i] == rightSum[i])
+                return i;
+        }
+        
+        return -1;
+    }
+}
+*/
