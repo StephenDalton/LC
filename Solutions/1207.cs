eefac6db9656178139f2b06ac8f4a84720b35d31
Stephen Dalton <stephen.dalt@gmail.com>
2022-07-27 07:42:47 -0400

    Update 1207.cs

--- Solutions/1207.cs
+++ Solutions/1207.cs
@@ -1,1 +1,26 @@
-blank
+//https://leetcode.com/problems/unique-number-of-occurrences/
+public class Solution {
+    public bool UniqueOccurrences(int[] arr) {
+        
+        Dictionary<int, int> dictionary = new Dictionary<int, int>();
+        HashSet<int> occurences = new HashSet<int>();
+        
+        foreach (int n in arr)
+        {
+            if (!dictionary.ContainsKey(n))
+                dictionary.Add(n, 1);
+            else
+                dictionary[n]++;
+        }
+        
+        foreach (var n in dictionary)
+        {
+            if (!occurences.Contains(n.Value))
+                occurences.Add(n.Value);
+            else
+                return false;
+        }
+        
+        return true;
+    }
+}
