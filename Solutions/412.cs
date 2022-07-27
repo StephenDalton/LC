993e22a0ee589a2689e20d243aae6f731a0475d5
Stephen Dalton <stephen.dalt@gmail.com>
2022-07-27 11:23:51 -0400

    Update 412.cs

--- Solutions/412.cs
+++ Solutions/412.cs
@@ -1,1 +1,20 @@
-blank
+//https://leetcode.com/problems/fizz-buzz/
+public class Solution {
+    public IList<string> FizzBuzz(int n) {
+        List<string> results = new List<string>();
+        
+        for (int i=1; i<=n; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+                results.Add("FizzBuzz");
+            else if (i % 3 == 0)
+                results.Add("Fizz");
+            else if (i % 5 == 0)
+                results.Add("Buzz");
+            else
+                results.Add(i.ToString());
+        }
+        
+        return results;
+    }
+}
