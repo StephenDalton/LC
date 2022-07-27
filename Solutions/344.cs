129d653e254789481f6036ff0432207995318993
Stephen Dalton <stephen.dalt@gmail.com>
2022-07-27 11:32:45 -0400

    Update 344.cs

--- Solutions/344.cs
+++ Solutions/344.cs
@@ -1,1 +1,30 @@
-blank
+//
+public class Solution {
+    public void ReverseString(char[] s) {
+        int left = 0;
+        int right = s.Length - 1;
+        
+        while(left < right)
+        {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            
+            left++;
+            right--;
+        }
+    }
+}
+
+/*
+public class Solution {
+    public void ReverseString(char[] s) {
+        for (int i=0; i<s.Length / 2; i++)
+        {
+            char temp = s[i];
+            s[i] = s[s.Length - i - 1];
+            s[s.Length - i - 1] = temp;
+        }
+    }
+}
+*/
