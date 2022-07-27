acc7be4b5c9953f3c8822da2c8155737c8bcab8f
Stephen Dalton <stephen.dalt@gmail.com>
2022-07-27 12:11:05 -0400

    Update 509.cs

--- Solutions/509.cs
+++ Solutions/509.cs
@@ -1,1 +1,39 @@
-blank
+//https://leetcode.com/problems/fibonacci-number/discuss/215992/Java-Solutions
+public class Solution {
+    public int Fib(int n) {
+        if (n <= 1)
+            return n;
+        
+        int[] fibCache = new int[n + 1];
+        fibCache[1] = 1;
+        
+        for(int i=2; i<=n; i++)
+        {
+            fibCache[i] = fibCache[i-1] + fibCache[i-2];
+        }
+        
+        return fibCache[n];
+    }
+}
+
+/*
+public class Solution {
+    public int Fib(int n) {
+        if (n <= 1)
+            return n;
+        
+        int a = 0;
+        int b = 1;
+        
+        for (int i=1; i<n; i++)
+        {
+            int sum = a + b;
+
+            a = b;
+            b = sum;
+        }
+        
+        return b;
+    }
+}
+*/
