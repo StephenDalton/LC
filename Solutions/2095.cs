a0b695c68d3097da8f2245e734ca96fab0a9dfae
Stephen Dalton <stephen.dalt@gmail.com>
2022-10-14 10:35:38 -0400

    Update 2095.cs

--- Solutions/2095.cs
+++ Solutions/2095.cs
@@ -1,1 +1,33 @@
-temp
+//
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode DeleteMiddle(ListNode head) {
+        if (head == null || head.next == null)
+            return null;
+
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null)
+        {
+            fast = fast.next.next;
+            prev = slow;
+            slow = slow.next;
+        }
+
+        prev.next = prev.next.next;
+
+        return head;
+    }
+}
